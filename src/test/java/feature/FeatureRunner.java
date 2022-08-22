060afdb051243620d74408f7d757373436863df0
Lalit Tewari <lalittew@gmail.com>
2022-08-22 21:24:17 +0100

    Final Commit

--- src/test/java/feature/FeatureRunner.java
+++ src/test/java/feature/FeatureRunner.java
@@ -5,7 +5,7 @@ import com.intuit.karate.junit4.Karate;
 import org.junit.runner.RunWith;
 
 @RunWith(Karate.class)
-@KarateOptions(features =  {"classpath:feature/getTesting.feature",
+@KarateOptions(features =  {"classpath:feature/GetTesting.feature",
                             "classpath:feature/PostTesting.feature",
                             "classpath:feature/PutTesting.feature"})
 public class FeatureRunner {
